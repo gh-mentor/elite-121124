959ccb2331b563355db0dbb6cf6e80d5472aa00f
Andrew Scoppa <andrew-scoppa@github.com>
2024-12-10 12:53:21 -0500

    Update

--- sync.ps1
+++ sync.ps1
@@ -1,4 +1,4 @@
-# This bash script uses git to sync changes between a local and remote GitHub repository on branch 'main'.
+# This powershell script uses git to sync changes between a local and remote GitHub repository on branch 'main'.
 # It assumes that the remote repository is already set up and that the local repository is already cloned.
 
 # Steps: pull changes from remote repository, stage all changes, commit changes with message 'Updated', push changes to remote repository on branch 'main'.
